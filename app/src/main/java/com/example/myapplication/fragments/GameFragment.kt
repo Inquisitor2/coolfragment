364bcdf7a27adc27a5cb8f197701d730b7362791
unknown <otari.tchanturia@btu.edu.ge>
2020-12-07 14:43:48 +0400

    first commit

---
+++ app/src/main/java/com/example/myapplication/fragments/GameFragment.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication.fragments
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class GameFragment: Fragment(R.layout.fragment_game) {
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
